fcdf4b8199862cf0fcb90ce1de3e14eb6979d5c1
WesleyCod <wesleypalov@yahoo.com>
2023-02-22 13:20:27 -0700

    first push

---
+++ script.js
@@ -0,0 +1,8 @@
+// Easy access to elements
+var searchHistoryList = $('#search-history-list');
+var searchCityInput = $("#search-city");
+var searchCityButton = $("#search-city-button");
+var clearHistoryButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemp = $("#current-temp");
+var currentHumidity = $("#current-humidity");
