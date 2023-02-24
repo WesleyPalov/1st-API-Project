03f2b875f677bfd30952602fdd7817967aba4c25
Thomas Wray <twray95@gmail.com>
2023-02-24 12:39:27 -0700

    one traffic tile now overlays onto the map

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,122 +9,128 @@ var currentTemp = $("#current-temp");
 var currentHumidity = $("#current-humidity");
 var currentWindSpeed = $("#current-wind-speed");
 var UVindex = $("#uv-index");
-// storing local storage 
+// storing local storage
 var locStor;
 
 var weatherContent = $("#weather-content");
 
-var currentDate = moment().format('L');
+var currentDate = moment().format("L");
 $("#current-date").text("(" + currentDate + ")");
 
 console.log(currentDate);
 // Get access to the OpenWeather API
-var urlWeather =  "https://api.openweathermap.org/data/2.5/weather?q=";
-var APIkey = "5806b5e472a87f66457f7aa46221f33b"
+var urlWeather = "https://api.openweathermap.org/data/2.5/weather?q=";
+var APIkey = "5806b5e472a87f66457f7aa46221f33b";
 
-searchCityButton.on("click", function (event){
-    event.preventDefault();
-    var searchValue = searchCityInput.val().trim();
-    if (searchValue === "") {
+searchCityButton.on("click", function (event) {
+  event.preventDefault();
+  var searchValue = searchCityInput.val().trim();
+  if (searchValue === "") {
     alert("Please enter City name know the current weather");
-   } else
-   console.log(searchValue);
-    currentWeatherRequest(searchValue);
-   locStor.push(searchValue);
-   weatherContent.removeClass("hide");
-   clearHistoryButton.removeClass("hide");
-   toLocalStore();
-   renderCities();
-})
+  } else console.log(searchValue);
+  currentWeatherRequest(searchValue);
+  locStor.push(searchValue);
+  weatherContent.removeClass("hide");
+  clearHistoryButton.removeClass("hide");
+  toLocalStore();
+  renderCities();
+});
 
 function currentWeatherRequest(searchValue) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    searchValue +
+    "&units=imperial&appid=" +
+    APIkey;
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial&appid=" + APIkey;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    currentCity.text(response.name);
+    currentCity.append("<small class='text-muted' id='current-date'>");
+    $("#current-date").text("(" + currentDate + ")");
+    currentCity.append(
+      "<img src='https://openweathermap.org/img/w/" +
+        response.weather[0].icon +
+        ".png' alt='" +
+        response.weather[0].main +
+        "' />"
+    );
+    currentTemp.text(response.main.temp);
+    currentTemp.append("&deg;F");
+    currentHumidity.text(response.main.humidity + "%");
+    currentWindSpeed.text(response.wind.speed + "MPH");
 
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+
+    var UVurl =
+      "https://api.openweathermap.org/data/2.5/uvi?&lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=" +
+      APIkey;
+    // AJAX Call for UV index
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-        currentCity.text(response.name);
-        currentCity.append("<small class='text-muted' id='current-date'>");
-        $("#current-date").text("(" + currentDate + ")");
-        currentCity.append("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='" + response.weather[0].main + "' />" )
-        currentTemp.text(response.main.temp);
-        currentTemp.append("&deg;F");
-        currentHumidity.text(response.main.humidity + "%");
-        currentWindSpeed.text(response.wind.speed + "MPH");
+      url: UVurl,
+      method: "GET",
+    }).then(function (response) {
+      console.log("UV call: ");
 
-        var lat = response.coord.lat;
-        var lon = response.coord.lon;
-        
-
-        var UVurl = "https://api.openweathermap.org/data/2.5/uvi?&lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
-        // AJAX Call for UV index
-        $.ajax({
-            url: UVurl,
-            method: "GET"
-        }).then(function(response){
-             console.log("UV call: ")
-          
-            UVindex.text(response.value);
-        });
+      UVindex.text(response.value);
+    });
+  });
 }
-)}; 
 
-function toLocalStore(){
-localStorage.setItem("cityName",  JSON.stringify(locStor));
+function toLocalStore() {
+  localStorage.setItem("cityName", JSON.stringify(locStor));
 }
 
 function fromLocalStore() {
-
-    var arr = localStorage.getItem("cityName");
-     if(arr) {
-        locStor = JSON.parse(arr)
-     }
-     else {
-        locStor = [];
-     }
+  var arr = localStorage.getItem("cityName");
+  if (arr) {
+    locStor = JSON.parse(arr);
+  } else {
+    locStor = [];
+  }
 }
 function renderCities() {
-    var tempTop = '<li class="list-group-item"> <button class="btn btn-info city-btn"  type="button" >'
-    var tempBut = "</button> </li>"
-    searchHistoryList.empty()
-for (let i = 0; i < locStor.length; i++) {
+  var tempTop =
+    '<li class="list-group-item"> <button class="btn btn-info city-btn"  type="button" >';
+  var tempBut = "</button> </li>";
+  searchHistoryList.empty();
+  for (let i = 0; i < locStor.length; i++) {
     var city = locStor[i];
-var finalTem = tempTop + city + tempBut;
-searchHistoryList.append(finalTem);
+    var finalTem = tempTop + city + tempBut;
+    searchHistoryList.append(finalTem);
+  }
 }
-}
-fromLocalStore()
+fromLocalStore();
 renderCities();
-clearHistoryButton.on("click", function(){
-    // Empty out the  city list array
-    locStor = [];
-    console.log(locStor);
-    renderCities();
-    // Update city list history in local storage
-    //listArray();
-    
+clearHistoryButton.on("click", function () {
+  // Empty out the  city list array
+  locStor = [];
+  console.log(locStor);
+  renderCities();
+  // Update city list history in local storage
+  //listArray();
+
   //  $(this).addClass("hide");
-})
+});
 console.log(locStor);
 
 // Clicking on a button in the search history sidebar
 // will populate the dashboard with info on that city
-searchHistoryList.on("click","btn.btn-info", function(event) {
-    console.log("dfd")
-     console.log($(this).data("value"));
-    var value = $(this).data("value");
-    //currentConditionsRequest(value);
-   // searchHistory(value); 
-
+searchHistoryList.on("click", "btn.btn-info", function (event) {
+  console.log("dfd");
+  console.log($(this).data("value"));
+  var value = $(this).data("value");
+  //currentConditionsRequest(value);
+  // searchHistory(value);
 });
-<<<<<<< HEAD
-=======
-console.log(tempTop);
-
->>>>>>> main
 
 //Michael's codebase
 var requestUrlNews =
@@ -153,8 +159,9 @@ var tileBL = tileFetchUrl + "426/777" + mapKey;
 var tileBR = tileFetchUrl + "427/777" + mapKey;
 var topRowEl = document.querySelector("#topRow");
 var bottomRowEl = document.querySelector("#bottomRow");
+var mapContainerEl = document.querySelector("#map-containernumerodos");
 
-function fetchTile(tileUrl) {
+function fetchMapTile(tileUrl) {
   fetch(tileUrl)
     .then(function (response) {
       return response;
@@ -163,17 +170,45 @@ function fetchTile(tileUrl) {
       console.log(data);
       var mapTile = document.createElement("img");
       mapTile.setAttribute("src", data.url);
-      console.log(tileUrl);
-      console.log(typeof tileUrl);
-      if (tileUrl.includes("776")) {
-        topRowEl.appendChild(mapTile);
-      } else {
-        bottomRowEl.appendChild(mapTile);
-      }
+      mapContainerEl.appendChild(mapTile);
     });
 }
 
-fetchTile(tileTL);
-fetchTile(tileTR);
-fetchTile(tileBL);
-fetchTile(tileBR);
+function fetchTrafficTile(tileUrl) {
+  fetch(tileUrl)
+    .then(function (response) {
+      return response;
+    })
+    .then(function (data) {
+      console.log(data);
+      var mapTile = document.createElement("img");
+      mapTile.setAttribute("src", data.url);
+      mapTile.classList.add("overlayTraffic");
+      mapContainerEl.appendChild(mapTile);
+    });
+}
+
+fetchMapTile(tileTL);
+fetchMapTile(tileTR);
+fetchMapTile(tileBL);
+fetchMapTile(tileBR);
+
+var trafficFetchUrl =
+  "https://api.tomtom.com/traffic/map/4/tile/flow/relative0/11/";
+var trafficTL = trafficFetchUrl + "426/776" + mapKey;
+var trafficTR = trafficFetchUrl + "427/776" + mapKey;
+
+fetchTrafficTile(trafficTL);
+fetchTrafficTile(trafficTR);
+
+// fetch(trafficTL)
+//   .then(function (response) {
+//     return response;
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//     var trafficTile = document.createElement("img");
+//     trafficTile.setAttribute("src", data.url);
+//     trafficTile.classList.add("overlayTraffic");
+//     topRowEl.appendChild(trafficTile);
+//   });
