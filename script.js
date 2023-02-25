9f6c7c93fd04f806e78e80c0b0fec77e2ae75c81
Michael Mount <116608765+MMount98@users.noreply.github.com>
2023-02-24 19:48:08 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Easy access to elements
-var searchHistoryList = $('#search-history-list');
-var searchCityInput = $("#search-city");
-var searchCityButton = $("#search-city-button");
-var clearHistoryButton = $("#clear-history");
-var currentCity = $("#current-city");
-var currentTemp = $("#current-temp");
-var currentHumidity = $("#current-humidity");
